f9eaabde7b417231e66ffd664cf571500171b802
hennaheinonen <henna.heinonen96@gmail.com>
2017-12-07 13:33:11 +0200

    Merkkijono-3
    Merkkijono-3

---
+++ string-handling/merkkijono-3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace merkkijono3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee käyttäjän syöttämästä syötteestä L-kirjainten lukumäärän");
+            Console.WriteLine("Syötä sana tai lause");
+
+            string userInput = Console.ReadLine();
+
+            int count = 0;
+
+            foreach (char ch in userInput) 
+            {
+                if (ch == 'l' || ch == 'L')
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"Syötteessä oli L-kirjaimia {count} määrä.");
+
+
+        }
+    }
+}
