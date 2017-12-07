8e6ea72e743e4ada583980e2d7b8b940448e57b5
hennaheinonen <henna.heinonen96@gmail.com>
2017-12-07 13:14:05 +0200

    Merkkijono-1
    Merkkijono-1

---
+++ string-handling/merkkijono-1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace merkkijono1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma ilmoittaa syötetystä syötteestä merkkien lukumäärän.");
+            Console.WriteLine("Syötä sana tai lause");
+
+            string userInput = Console.ReadLine();
+
+            Console.WriteLine($"Syötteessä oli {userInput.Length} merkkiä.");
+        }
+    }
+}
