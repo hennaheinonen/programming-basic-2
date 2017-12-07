de921e86e441d99fc72f003bb7bc1d09bb9365a3
hennaheinonen <henna.heinonen96@gmail.com>
2017-12-07 14:15:31 +0200

    Itsearviointitehtävä-2
    Itsearviointitehtävä-2

---
+++ itsearviointitehtävät/itsearviointitehtävä-2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace itsearviointitehtävä2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma kysyy käyttäjältä sanan tai lauseen ja tulostaa sen niin monta kertaa näytölle kuin sanassa on kirjaimia.");
+            Console.WriteLine("Söytä sana tai lause");
+
+            string userInput = Console.ReadLine();
+
+            for (int i = 0; i < userInput.Length; i++) 
+            {
+                Console.WriteLine($"{i}. {userInput}");
+            }
+        }
+    }
+}
