43295a56194ed3ead4551392281ddea873d5274d
hennaheinonen <henna.heinonen96@gmail.com>
2017-12-07 13:47:45 +0200

    Merkkijono-4
    merkkijono-4

---
+++ string-handling/merkkijono-4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace merkkijono4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tarkastaa onko käyttäjältä kysytty sana tai luku palintromi");
+            Console.WriteLine("Syötä sana");
+
+            string userInput = Console.ReadLine();
+            string reverse = "";
+            int i = userInput.Length;
+
+            for (int j = i - 1; j >= 0; j--) 
+            {
+                reverse = reverse + userInput[j];
+            }
+
+            if (reverse == userInput) 
+            {
+                Console.WriteLine($"Sana {userInput} on palintromi");
+            }
+            else 
+            {
+                Console.WriteLine($"Sana {userInput} ei ole palintromi");
+            }
+        }
+    }
+}
