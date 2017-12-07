561f75b72fd9a440222a7e5ff0ab25787d618bad
hennaheinonen <henna.heinonen96@gmail.com>
2017-12-07 14:05:24 +0200

    Vokaalilaskuri
    vokaalilaskuri

---
+++ string-handling/vokaalilaskuri/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace vokaalilaskuri
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee käyttäjän syöttämästä tiedosta vokaalien lukumäärän");
+            Console.WriteLine("Syötä sana");
+
+            string userInput = Console.ReadLine();
+
+            int count = 0;
+
+            foreach ( char ch in userInput) 
+            {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'y' || ch == 'ä' || ch == 'ö') 
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"Syötteessä oli {count} vokaalia.");
+        }
+    }
+}
