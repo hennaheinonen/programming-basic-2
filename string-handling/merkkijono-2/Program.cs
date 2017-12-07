e551384ad7578c86d80644c1187b0562c8901697
hennaheinonen <henna.heinonen96@gmail.com>
2017-12-07 13:21:43 +0200

    Merkkijono-2
    merkkijono-2

---
+++ string-handling/merkkijono-2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace merkkijono2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma muuttaa syötteestä k-kirjaimet @-merkiksi");
+            Console.WriteLine("Syötä sana tai lause");
+
+            string userInput = Console.ReadLine();
+
+            userInput = userInput.Replace("e", "@");
+
+            Console.WriteLine($"{userInput}");
+        }
+    }
+}
